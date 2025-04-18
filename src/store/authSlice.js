0ba0a3fda4e8f8877fef44c143575cb0c2010b6e
Atharvatathe <atharva.tathe55@gmail.com>
2025-04-18 07:59:45 +0530

    worked on header footer and button input component

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -11,7 +11,7 @@ const authSlice = createSlice({
   reducers: {
     login: (state, action) => {
       state.status = true;
-      state.userData = action.payload;
+      state.userData = action.payload.userData;
     },
     logout: (state) => {
       state.status = false;
@@ -20,5 +20,6 @@ const authSlice = createSlice({
   },
 });
 
-export default authSlice.reducer;
 export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
